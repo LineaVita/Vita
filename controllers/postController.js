b485ce93ed211c2df4241aa9481f9ee7bd960130
brian <brian@treegecko.com>
2015-12-19 21:47:21 -0500

    work on post

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,2 +1,37 @@
-vitaApp.controller('postController', ['$scope', function($scope) {
+vitaApp.controller('postController', ['$scope', '$routeParams', '$location', 
+  function($scope, $routeParams, $location) {
+  
+  $scope.newPost = function(){
+    var post = {};
+    
+    post.DateTime = Date.now();   
+    post.Text = "Testing";
+    post.getDateString = function() {
+      var d = new Date(this.DateTime);
+      return d.toLocaleDateString() + " " + d.toLocaleTimeString()
+    }
+       
+    return post;
+  }
+  
+  $scope.loadPost = function(postId) {
+    var post = $scope.newPost();
+    
+    //todo = load values;
+  }
+  
+  $scope.savePost = function(post) {
+    //TODO - save the post via service
+    
+    $location.path('/home');
+  }
+  
+  
+  //Set the current post to work with
+  if ($routeParams.postId == null) {
+    $scope.Post = $scope.newPost();
+  } else {
+    $scope.Post = $scope.loadPost($routeParams.postId);
+  }  
+  
 }]);
