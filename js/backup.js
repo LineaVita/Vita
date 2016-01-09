91183a31b1f8284e5d93068e739473c7d31c9f70
brian <brian@treegecko.com>
2016-01-09 00:40:31 -0500

    more angular material

--- js/backup.js
+++ js/backup.js
@@ -21,3 +21,9 @@
         </div>
       </div>
     </nav>
+            
+            
+    <!-- Set up metadata -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">            
