b3215691a4035f23d8d04bf6dc15dbd00903f535
brian <brian@treegecko.com>
2016-01-20 08:26:41 -0500

    aws sdk work

--- services/AWSService.js
+++ services/AWSService.js
@@ -25,6 +25,7 @@ function($rootScope, $q, configService) {
       accessKeyId: awsService.Configuration.AWSKey, 
       secretAccessKey: awsService.Configuration.AWSSecret };
     
+    awsService.Enabled = true;
     //TODO mark as ready and deal with stuff that came in before ready.
     //Check in database for new records? 
     
