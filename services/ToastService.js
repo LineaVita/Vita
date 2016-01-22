1eeea17c94c828697114d672036ab2f1a737aed9
brian <brian@treegecko.com>
2016-01-22 08:22:35 -0500

    Moved toast to toast service

--- services/ToastService.js
+++ services/ToastService.js
@@ -1,7 +1,31 @@
 vitaApp.factory('ToastService', ['$mdToast', 
 function($mdToast) {
   var toastService = {};
-    
+  
+  toastService.Last = {
+      bottom: false,
+      top: true,
+      left: false,
+      right: true 
+    };
+  
+  toastService.toastPosition = angular.extend({}, toastService.last);
+  
+  toastService.getToastPosition = function() {
+    toastService.sanitizePosition();
+    return Object.keys(toastService.toastPosition)
+      .filter(function(pos) { return toastService.toastPosition[pos]; })
+      .join(' ');
+  };
+  
+  toastService.ShowToast = function(message) {
+    $mdToast.show(
+      $mdToast.simple()
+        .textContent(message)
+        .position(toastService.getToastPosition())
+        .hideDelay(3000)
+    );
+  };
   
   return toastService;
 }]);
