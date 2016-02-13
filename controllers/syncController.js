41adb46192acbe1df60d20e999d7a168f898a687
brian <brian@treegecko.com>
2016-02-13 10:07:21 -0500

    setting up framework for AWS Sync

---
+++ controllers/syncController.js
@@ -0,0 +1,7 @@
+vitaApp.controller('syncController', ['$scope', '$routeParams', '$location', 'PostService', 'GPSService', 'FileService', 'PlaceService',
+function($scope, $routeParams, $location, postService, gpsService, fileService, placeService) {
+
+
+
+}]);
+                                      
