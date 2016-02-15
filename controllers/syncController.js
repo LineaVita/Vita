c03212e77d0f9838163abfdcea4cbaeb57eb9541
brian <brian@treegecko.com>
2016-02-15 08:45:40 -0500

    work on syncing

--- controllers/syncController.js
+++ controllers/syncController.js
@@ -1,7 +1,32 @@
-vitaApp.controller('syncController', ['$scope', '$routeParams', '$location', 'PostService', 'GPSService', 'FileService', 'PlaceService',
-function($scope, $routeParams, $location, postService, gpsService, fileService, placeService) {
+vitaApp.controller('syncController', ['$scope', '$routeParams', '$location', 'PostService', 'GPSService', 'FileService', 'PlaceService', 'ConfigurationService',
+function($scope, $routeParams, $location, postService, gpsService, fileService, placeService, configService) {  
+  
+  $scope.LastSyncTime = null;
+  $scope.LastSyncTimeString = null;
+  $scope.ProgressMode = null;
+  $scope.Syncing = false;
+    
+  $scope.GetLastSyncTimeString = function() {
+    configService.GetSettingByName('LastAWSSyncTime')
+    .then(function (rawValue) {
+      if (rawValue != null) {
+        var d = new Date(rawValue);
+        $scope.LastSyncTime = d;
+        $scope.LastSyncTimeString = "Last sync time: " + d.toLocaleDateString() + " " + d.toLocaleTimeString();
+      } else {
+        $scope.LastSyncTimeString = "Last sync time: Never synced";
+      }      
+    });
+  }
+  
+  $scope.SyncNow = function () {
+    $scope.ProgressMode = "indeterminate";
+    $scope.Syncing = true;
 
-
+  }
+  
+  //Run setup functions
+  $scope.GetLastSyncTimeString();
 
 }]);
                                       
